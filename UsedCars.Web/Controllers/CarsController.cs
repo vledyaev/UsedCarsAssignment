5de6456892cd2f6319fdebac71291044554960cc
Vladimir Ledyaev <vladimir.ledyaev@crossix.veeva.com>
2022-04-03 20:35:55 +0300

    Create base project structure using Asp.net core project template and create-react-app with typescript template.

---
+++ UsedCars.Web/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsedCars.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CarsController: ControllerBase
+{
+    private readonly ILogger<CarsController> _logger;
+
+    public CarsController(ILogger<CarsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<int> Get()
+    {
+        return Enumerable.Range(1, 5);
+    }
+}
