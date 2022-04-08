df570a380cc70146699ded9c582d1bf39eee753b
Vladimir Ledyaev <vladimir.ledyaev@crossix.veeva.com>
2022-04-08 15:00:55 +0300

    Remove seed file as all needed data is stored in the migration.

--- UsedCars.DAL/UsedCarsContext.cs
+++ UsedCars.DAL/UsedCarsContext.cs
@@ -25,40 +25,4 @@ public class UsedCarsContext: DbContext
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={_dbPath}");
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        using var reader = new StreamReader("warehouses.json");
-
-        var json = reader.ReadToEnd();
-        var warehouses = JsonConvert.DeserializeObject<List<WarehouseModel>>(json);
-
-        if (warehouses != null)
-        {
-            builder.Entity<Warehouse>()
-                .HasData(warehouses.Select(x => new Warehouse
-                {
-                    Id = x._Id,
-                    Latitude = x.Location.Lat,
-                    Longitude = x.Location.Long,
-                    Name = x.Name,
-                    Location = x.Cars.Location
-                }));
-
-            builder.Entity<Car>()
-                .HasData(warehouses.SelectMany(warehouse => warehouse.Cars.Vehicles.Select(car => new Car
-                {
-                    Id = car._Id,
-                    WarehouseId = warehouse._Id,
-                    Licensed = car.Licensed,
-                    Make = car.Make,
-                    Model = car.Model,
-                    Price = car.Price,
-                    Year = car.Year_Model,
-                    DateAdded = car.Date_Added
-                })));
-        }
-
-        base.OnModelCreating(builder);
-    }
 }
